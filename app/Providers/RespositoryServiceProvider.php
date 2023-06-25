c000b972ae7ab7a77787414b93e9bb2efb123121
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2023-06-25 10:05:14 +0800

    Setup RepositoryServiceProvider

---
+++ app/Providers/RespositoryServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RespositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Put all interface-repository bindings here
+    }
+}
