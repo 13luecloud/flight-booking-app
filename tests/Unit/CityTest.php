2bd82eaf3a0cc7bc63fde171a9d1af4ccd5cc818
Donna-Dawn Dagcuta <d.dawnseadd@gmail.com>
2023-06-26 20:38:59 +0800

    Updated City with initial unit test

--- tests/Unit/CityTest.php
+++ tests/Unit/CityTest.php
@@ -2,18 +2,33 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Log;
+
+use App\Http\Repositories\City\CityRepository;
 use App\Models\City; 
 
 class CityTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    private $repository;
+    public function setUp(): void 
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->repository = app(CityRepository::class);
     }
+
+    public function test_successfully_create_cities()
+    {
+        $cities = City::factory()->count(3)->make();
+        foreach($cities as $city) {
+            $createdCity = $this->repository->createCity($city->toArray());
+            log::info($createdCity);
+            $this->assertEquals($city['name'], $createdCity->name);
+        }
+    }
+
 }
